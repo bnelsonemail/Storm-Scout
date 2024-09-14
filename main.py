e57289f266eaf2f6c0817235dd8c9253f817da17
bnelson <bnelsonemail@icloud.com>
2024-09-13 23:32:28 -0400

    revised get_lat_lon function

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Weather:
     Class that uses an API call to collect weather data based on user input of City, State Code and Country Code (ie: US, GB, etc.)
     """
     
-    def __init__(self, city_name, state_code, limit, lang, country_code):
+    def __init__(self, city_name, state_code, country_code):
         """
         Initializes the Weather class with city name, state code, and country code.
 
@@ -26,9 +26,9 @@ class Weather:
         self.city_name = city_name
         self.state_code = state_code
         self.country_code = country_code
-        self.limit = limit,
-        self.lang = lang
 
+        if not self._api_key:
+            raise ValueError("API key not found.  Please set the API key in the .env file")
 
     def get_lat_lon(self):
         """
@@ -40,9 +40,10 @@ class Weather:
         # Format city and state for the URL
         city_formatted = self.city_name.replace(" ", "%20").lower()
         state_formatted = self.state_code.lower()
+        country_formatted = self.country_code.lower()
 
         # Build the Zippopotam.us API request
-        url = f"http://api.zippopotam.us/us/{state_formatted}/{city_formatted}"
+        url = f"http://api.zippopotam.us/{country_formatted}/{state_formatted}/{city_formatted}"
         
         try:
             response = requests.get(url)
@@ -78,14 +79,14 @@ class Weather:
             'lat': lat,
             'lon': lon,
             'appid': self._api_key,
-            'units': 'standard'
+            'units': 'imperial'
         }
     
         try:
             response = requests.get(self.base_weather_url, params=params)
             response.raise_for_status() # Raises an error if the request was unsuccessful
             return response.json()
-        except requests.exeptions.GTTPError as http_err:
+        except requests.exeptions.HTTPError as http_err:
             print(f"HTTP error occurred: {http_err}")
         except Exception as err:
             print(f"Error occurred: {err}")
@@ -110,7 +111,7 @@ class Weather:
             print(f"Temperature: {temp}°F")
             print(f"Condition: {description.capitalize()}")
         else:
-            print("No wather data available.")
+            print("No weather data available.")
         
 
     
